8b91dc444b6d9d13e8445bbb5b605cb05f2068aa
mithunadhiakry <mithun.cse521@gmail.com>
2017-09-17 15:52:35 +0600

    Stock View Update

--- backend/views/stock-view/index.php
+++ backend/views/stock-view/index.php
@@ -49,6 +49,12 @@ $this->params['breadcrumbs'][] = $this->title;
           <?= GridView::widget([
             'dataProvider' => $dataProvider,
             'filterModel' => $searchModel,
+            'rowOptions'=>function($model){
+                if($model->available <= 10){
+                    return ['class' => 'red'];
+                }
+            },
+
             'columns' => [
                 #['class' => 'yii\grid\SerialColumn'],
 
